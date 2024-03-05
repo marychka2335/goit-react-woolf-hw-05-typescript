f43adc0f65e9568390e94fe25e78134f4150d29f
marychka2335 <marychka2335@icloud.com>
2024-03-05 07:02:24 +0200

    fixed changes

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -41,10 +41,13 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
-    id: "first",
-  });
+  // const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+  //   id: "first",
+  // });
 
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>(
+    {} as SelectedMenu
+  );
   const menuContextAction = useMemo(
     () => ({
       onSelectedMenu: setSelectedMenu,
